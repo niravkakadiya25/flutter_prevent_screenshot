4c561f99fefe67de5abe1e52d2a193a3d9e55df8
Nirav <nirav@ayoo.care>
2024-06-29 10:02:40 +0530

    initial

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_prevent_screenshot/NoScreenshotPlugin.h>)
+#import <flutter_prevent_screenshot/NoScreenshotPlugin.h>
+#else
+@import flutter_prevent_screenshot;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [NoScreenshotPlugin registerWithRegistrar:[registry registrarForPlugin:@"NoScreenshotPlugin"]];
+}
+
+@end
---
+++ ios/Classes/DisableScreenshots.m
@@ -0,0 +1,15 @@
+#import "DisableScreenshotsPlugin.h"
+#if __has_include(<flutter_prevent_screenshot/flutter_prevent_screenshot-Swift.h>)
+#import <flutter_prevent_screenshot/flutter_prevent_screenshot-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "flutter_prevent_screenshot-Swift.h"
+#endif
+
+@implementation NoScreenshotPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftNoScreenshotPlugin registerWithRegistrar:registrar];
+}
+@end
